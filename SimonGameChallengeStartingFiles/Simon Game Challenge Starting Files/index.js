bac8de633fa6f436823993a7ecc902d775bf8a05
Aqil Nu'man <aqilnuman23@gmail.com>
2023-09-11 02:53:51 +0800

    Update index.js

--- SimonGameChallengeStartingFiles/Simon Game Challenge Starting Files/index.js
+++ SimonGameChallengeStartingFiles/Simon Game Challenge Starting Files/index.js
@@ -4,5 +4,4 @@ function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     // why times 4? because it would round number down than or equal to the number which is <4.. So we would get a random 0-3 number.
 
-
 }
