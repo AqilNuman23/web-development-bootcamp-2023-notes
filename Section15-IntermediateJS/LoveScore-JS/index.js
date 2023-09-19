1cb279870e5d2fa8384c9eb375cb6fbe3f707bc0
Aqil Nu'man <130033381+AqilNuman23@users.noreply.github.com>
2023-09-19 10:18:51 +0800

    Update index.js

--- Section15-IntermediateJS/LoveScore-JS/index.js
+++ Section15-IntermediateJS/LoveScore-JS/index.js
@@ -1,21 +1,38 @@
 const prompt = require("prompt-sync")();
 
 const perInfo = () => {
-    prompt("what is your name? ");
-    prompt("what is their name? ");
+    const yourName = prompt("what is your name? ");
+    const theirName = prompt("what is their name? ");
+
+    return {yourName, theirName};
 };
 
 const loveGame = () => {
     var loveScore = Math.floor(Math.random() * 100) + 1;
-    console.log("Your love score is " + loveScore + "%");
+    let indicaterMessage = "";
+    // love score indicater
+    if (loveScore === 100) {
+        indicaterMessage = "Your love is perfect, congratulation";
+    } else if (loveScore > 70) {
+        indicaterMessage = "Your love is gonna be cool, congratulation";
+    } else if (loveScore > 50) {
+        indicaterMessage = "Your love is just fine";
+    } else if (loveScore <= 50) {
+        indicaterMessage = "You may need some luck and faith in it";
+    } else {
+        indicaterMessage = "You might consider to chase another person :|";
+    }
+    return {indicaterMessage, loveScore};
 };
 
 const startGame = () => {
     while (true) {
-        perInfo();
-        loveGame();
+        const perInfoResult = perInfo();
+        const loveGameResult = loveGame();
 
-        const playAgain = prompt("Wanna play again? (y/n)");
+        console.log("Your love score is " + loveGameResult.loveScore + "% \n" + perInfoResult.yourName + " and " + perInfoResult.theirName + ", " + loveGameResult.indicaterMessage);
+
+        const playAgain = prompt("Wanna play again? (y/n): ");
         if (playAgain != "y") break;
     }
 };
